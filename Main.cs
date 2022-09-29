9c6d1612f22ff3c414c8d25577e7480f04849617
Shurikino <contu.alexander@gmail.com>
2022-09-29 23:29:29 +0200

    first commit

---
+++ Main.cs
@@ -0,0 +1,31 @@
+﻿using GameStates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MainMenu;
+
+namespace Main
+{
+    class game
+    {
+        static void Main(string[] args)
+        {
+            Menu firstScreen = new Menu();
+            firstScreen.menuText();
+
+
+
+
+
+
+
+        }
+    }
+}
+    
+    
+       
+   
+
---
+++ MainMenu.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameStates;
+
+namespace MainMenu
+{
+    public class Menu
+    {
+
+
+        public void menuText()
+        {
+            Console.WriteLine("BATTLE OF STAMFORD BRIDGE");
+            Console.WriteLine("Press p to Play, press q to Quit");
+            string userDecision = Console.ReadLine()!;
+            
+            if (userDecision.ToUpper() == "P")
+            {
+                Console.WriteLine("game launched");
+            }
+            else if(userDecision.ToUpper() == "Q")
+            {
+                Console.WriteLine("game closed");
+                Environment.Exit(0);
+            }
+            else 
+            { 
+                Console.WriteLine("choose again");
+                menuText();
+            }
+            
+
+                
+                
+            
+                
+            
+            
+            
+           
+            
+
+
+
+
+
+
+
+        }
+    }
+}
